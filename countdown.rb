4837eead6c68a4850ac8dc9d1e960f7216997635
Naveed Shah <naveed@tayybah.com>
2016-07-28 10:12:39 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(argument)
+
+  while argument != 0
+    puts "#{argument} SECOND(S)!"
+    argument = argument -1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(argument)
+  while argument != 0
+    puts "#{argument} SECOND(S)!"
+    argument = argument -1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
